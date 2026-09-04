b7c36b86f3ba84dbd09f2b0bafc97ca6ce7f9f2d
Francis-Ezekiel-Fernandez <fernandez.fe@obmontessori.edu.ph>
2026-09-04 14:51:22 +0800

    1

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 from pyscript import document, display
 display(target="div1")
 
-name = "Name:Francis Ezekiel Fernandez"
+name = "Name: Francis Ezekiel Fernandez"#string
 
-display(name, target="div1")
+display(name, target="div2")
 
-name = "Name:Francis Ezekiel Fernandez"
+age = "Age: 14"#integer
 
-display(name, target="div1")
+display(age, target="div3")
+
+height14 = "Height: 157.48 cm"#float
+
+display(height14, target="div4")
+
+countries_list = ["Japan", "Malaysia", "Germany", "Korea"]#list
+
+display(countries_list, target="div5")
+
+student_type = "false"#boolean
+
+display(student_type, target="div6")
+
+aboutme ={
+        "Favorite Color": "Purple",
+        "Car Brand": "Honda",
+        "Shoe Size": "8.5 US Mens",
+        "Best Friend": "Ary" ,
+}#dict
+
+display(aboutme, target="div7")
+
+fav_fruits = set(['Bananas','Avocados','Kiwis','Dragonfruit','Apples']) #set
+
+display(fav_fruits, target="div8")
+
+days_of_the_week = tuple(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) #tuple
+
+display(days_of_the_week, target="div9")
